56463885412c235c2d59da021ccafdb7d11b10c9
Meet Jethwa <meetjethwa3@gmail.com>
2023-07-12 22:04:19 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-
+import openai
 from PyPDF2 import PdfReader
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.text_splitter import CharacterTextSplitter
