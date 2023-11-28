c57fcde717f5719552f668801244eedcd7ca10d6
Meet Jethwa <meetjethwa3@gmail.com>
2023-11-28 14:14:23 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,70 +1,73 @@
+#pip install streamlit langchain openai faiss-cpu tiktoken
+
 import streamlit as st
 from streamlit_chat import message
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import ConversationalRetrievalChain
+from langchain.document_loaders.csv_loader import CSVLoader
 from langchain.vectorstores import FAISS
 import tempfile
-import os
-import pandas as pd
-import openai
 
-output_csv = "C:\\Users\\mejethwa\\Downloads\\PDF_LLM-main\\PDF_LLM-main\\example_file.csv"
-# Set up OpenAI API Key
-# openai_api_key = st.sidebar.text_input(
-#     label="#### Your OpenAI API key 👇",
-#     placeholder="Paste your OpenAI API key here",
-#     type="password")
-openai.api_key = st.secrets["OPENAI_API_KEY"]
 
-# if not openai_api_key:
-#     st.sidebar.warning("Please enter your OpenAI API key.")
-#  # Setting the API key for the current session
+user_api_key = st.sidebar.text_input(
+    label="#### Your OpenAI API key 👇",
+    placeholder="Paste your openAI API key, sk-",
+    type="password")
 
-uploaded_file = st.sidebar.file_uploader("Upload CSV or Excel", type=["csv", "xlsx"])
+uploaded_file = st.sidebar.file_uploader("upload", type="csv")
 
-if uploaded_file:
-    try:
-        # Determine the file type and read accordingly
-        if uploaded_file.type == "text/csv":
-            df = pd.read_csv(uploaded_file)
-        elif uploaded_file.type == "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet":
-            df = pd.read_excel(uploaded_file)
-            df = df.to_csv(output_csv, index=False)
-        else:
-            st.error("Unsupported file type")
-            raise Exception("Unsupported file type")
+if uploaded_file :
+    with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
+        tmp_file.write(uploaded_file.getvalue())
+        tmp_file_path = tmp_file.name
 
-        # Assuming data to be in the first column
-        data = df[df.columns[0]].tolist()
+    loader = CSVLoader(file_path=tmp_file_path, encoding="utf-8")
+    data = loader.load()
 
-        embeddings = OpenAIEmbeddings()
-        vectorstore = FAISS.from_documents(data, embeddings)
+    embeddings = OpenAIEmbeddings()
+    vectors = FAISS.from_documents(data, embeddings)
 
-        chain = ConversationalRetrievalChain.from_llm(
-            llm=ChatOpenAI(temperature=0.0, model_name='gpt-3.5-turbo'),
-            retriever=vectorstore.as_retriever())
+    chain = ConversationalRetrievalChain.from_llm(llm = ChatOpenAI(temperature=0.0,model_name='gpt-3.5-turbo', openai_api_key=user_api_key),
+                                                                      retriever=vectors.as_retriever())
 
-        if 'history' not in st.session_state:
-            st.session_state['history'] = []
+    def conversational_chat(query):
+        
+        result = chain({"question": query, "chat_history": st.session_state['history']})
+        st.session_state['history'].append((query, result["answer"]))
+        
+        return result["answer"]
+    
+    if 'history' not in st.session_state:
+        st.session_state['history'] = []
 
-        # Chat interface
-        container = st.container()
-        with container:
-            with st.form(key='my_form', clear_on_submit=True):
-                user_input = st.text_input("Query:", placeholder="Talk about your csv data here :)", key='input')
-                submit_button = st.form_submit_button(label='Send')
+    if 'generated' not in st.session_state:
+        st.session_state['generated'] = ["Hello ! Ask me anything about " + uploaded_file.name + " 🤗"]
 
-            if submit_button and user_input:
-                output = chain({"question": user_input, "chat_history": st.session_state['history']})
-                st.session_state['history'].append((user_input, output["answer"]))
+    if 'past' not in st.session_state:
+        st.session_state['past'] = ["Hey ! 👋"]
+        
+    #container for the chat history
+    response_container = st.container()
+    #container for the user's text input
+    container = st.container()
 
-        response_container = st.container()
+    with container:
+        with st.form(key='my_form', clear_on_submit=True):
+            
+            user_input = st.text_input("Query:", placeholder="Talk about your csv data here (:", key='input')
+            submit_button = st.form_submit_button(label='Send')
+            
+        if submit_button and user_input:
+            output = conversational_chat(user_input)
+            
+            st.session_state['past'].append(user_input)
+            st.session_state['generated'].append(output)
+
+    if st.session_state['generated']:
         with response_container:
-            for i, (user_msg, bot_msg) in enumerate(st.session_state['history']):
-                message(user_msg, is_user=True, key=f'user_{i}', avatar_style="big-smile")
-                message(bot_msg, key=f'bot_{i}', avatar_style="thumbs")
-    except Exception as e:
-        st.error(f"An error occurred: {e}")
-
-
+            for i in range(len(st.session_state['generated'])):
+                message(st.session_state["past"][i], is_user=True, key=str(i) + '_user', avatar_style="big-smile")
+                message(st.session_state["generated"][i], key=str(i), avatar_style="thumbs")
+                
+#streamlit run tuto_chatbot_csv.py
