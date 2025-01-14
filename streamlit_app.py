e389921773a412f2390f9e5a4ed341dc9a60618a
Meet Jethwa <meetjethwa3@gmail.com>
2025-01-14 12:15:42 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,11 +2,11 @@
 
 import streamlit as st
 from streamlit_chat import message
-from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.chat_models import ChatOpenAI
-from langchain.chains import ConversationalRetrievalChain
-from langchain.document_loaders.csv_loader import CSVLoader
-from langchain.vectorstores import FAISS
+from langchain-community.embeddings.openai import OpenAIEmbeddings
+from langchain-community.chat_models import ChatOpenAI
+from langchain-community.chains import ConversationalRetrievalChain
+from langchain-community.document_loaders.csv_loader import CSVLoader
+from langchain-community.vectorstores import FAISS
 import tempfile
 
 
